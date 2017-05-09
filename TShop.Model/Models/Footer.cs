10618d959d79f64e6aa9838b1f436fa9d1ff4160
VBT\VBT <vubatientien@gmail.com>
2017-05-09 16:43:32 +0700

    Commit TShop.Model

---
+++ TShop.Model/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TShop.Model.Model
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
