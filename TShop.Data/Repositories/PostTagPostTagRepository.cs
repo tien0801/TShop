bf4960d280ed5fd1ff725a94543ecfea7f66a409
DESKTOP-OEMDB47\VBT <vubatientien@gmail.com>
2017-09-01 15:17:27 +0700

    update Data

--- TShop.Data/Repositories/PostTagPostTagRepository.cs
+++ TShop.Data/Repositories/PostTagPostTagRepository.cs
@@ -6,7 +6,7 @@ namespace TShop.Data.Repositories
     public interface IPostTagRepository : IRepository<PostTag>
     { }
 
-    public class PostTagRepository : RepositoryBase<PostTag>, IPostRepository
+    public class PostTagRepository : RepositoryBase<PostTag>, IPostTagRepository
     {
         public PostTagRepository(IDbFactory dbFactory) : base(dbFactory)
         {
