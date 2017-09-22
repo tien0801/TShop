cea273eeb5933ac9c6127d56841a06de332e5a05
DESKTOP-OEMDB47\VBT <vubatientien@gmail.com>
2017-09-22 11:55:59 +0700

    add notification service - bai 27

---
+++ TShop.Web/app/shared/services/notificationService.js
@@ -0,0 +1,41 @@
+﻿(function (app) {
+    app.factory('notificationService', notificationService);
+    function notificationService() {
+        toastr.options = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "showDuration": 200,
+            "hideDuration": 500,
+            "timeOut": 2000,
+            "extendedTimeOut": 1000
+        };
+       
+        function displaySuccess(message){
+            toastr.success(message);
+        }
+
+        function displayError(message){
+            if(Array.isArray(error)){
+                error.each(function(err){
+                    toastr.error(err);
+                })
+            }
+            else{
+                toastr.error(error);
+            }
+        }
+        function displayWarning(message){
+            toastr.warning(message);
+        }
+        function displayInfo(message){
+            toastr.info(message);
+        }
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo
+        }
+    }
+})(angular.module('tshop.common'));
