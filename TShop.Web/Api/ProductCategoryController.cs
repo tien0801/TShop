412f0b2033391283894b718c447653abd97144f6
DESKTOP-OEMDB47\VBT <vubatientien@gmail.com>
2017-09-24 23:36:18 +0700

    update form and auto make seo title service

--- TShop.Web/Api/ProductCategoryController.cs
+++ TShop.Web/Api/ProductCategoryController.cs
@@ -16,6 +16,7 @@ namespace TShop.Web.Api
     [RoutePrefix("api/ProductCategory")]
     public class ProductCategoryController : ApiControllerBase
     {
+        #region Initialize
         private IProductCategoryService _productCategoryService;
 
         public ProductCategoryController(IErrorService errorService, IProductCategoryService ProductCategoryService)
@@ -23,6 +24,7 @@ namespace TShop.Web.Api
         {
             this._productCategoryService = ProductCategoryService;
         }
+        #endregion
 
         [Route("getallparents")]
         [HttpGet]
@@ -35,6 +37,22 @@ namespace TShop.Web.Api
                 var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
 
                 var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
                 return response;
             });
         }
@@ -82,6 +100,7 @@ namespace TShop.Web.Api
                 {
                     var newProductCategory = new ProductCategory();
                     newProductCategory.UpdateProductCategory(productCategoryVm);
+                    newProductCategory.CreatedDate = DateTime.Now;
 
                     _productCategoryService.Add(newProductCategory);
                     _productCategoryService.Save();
@@ -92,5 +111,34 @@ namespace TShop.Web.Api
                 return response;
             });
         }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCategoryVm.ID);
+                    dbProductCategory.UpdateProductCategory(productCategoryVm);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+                return response;
+            });
+        }
+
     }
 }
--- TShop.Web/app/shared/services/apiService.js
+++ TShop.Web/app/shared/services/apiService.js
@@ -8,7 +8,8 @@
     function apiService($http, notificationService) {
         return {
             get: get,
-            post: post
+            post: post,
+            put: put
         }
 
         function post(url, data, success, failure) {
@@ -18,7 +19,20 @@
                 console.log(error.status);
                 if (error.status === 401) {
                     notificationService.displayError('Authenticate is required');
-                } else if(failure != null) {
+                } else if (failure != null) {
+                    failure(error);
+                }
+            });
+        }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status);
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required');
+                } else if (failure != null) {
                     failure(error);
                 }
             });
